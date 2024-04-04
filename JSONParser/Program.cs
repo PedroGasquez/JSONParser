b7ecb649f6e90bf1f805e91ca172ca8d3ddfe1e3
Pedro Henrique <105076457+Stal91@users.noreply.github.com>
2024-04-04 12:35:15 -0300

    Add files via upload

---
+++ JSONParser/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+
+string jsonData = File.ReadAllText(@"C:\Users\phlga\OneDrive\Documentos\dev\JSONParser\invalid.json");
+
+if (IsValidJson(jsonData))
+{
+    Console.WriteLine("0");
+}
+else
+{
+    Console.WriteLine("1");
+}
+
+
+
+bool IsValidJson(string jsonString)
+{
+    
+        jsonString = jsonString.Trim();
+
+        if (jsonString.StartsWith("{") && jsonString.EndsWith("}"))
+        {
+            jsonString = jsonString.Substring(1, jsonString.Length - 2).Trim();
+            
+            string[] keyValuePairs = jsonString.Split(',');
+
+            foreach (var pair in keyValuePairs)
+            {
+                string[] parts = pair.Split(":");
+                if (parts.Length != 2)
+                {
+                    return false;
+                }
+                string key = parts[0].Trim();
+                string value = parts[1].Trim();
+
+                
+                if (!key.StartsWith("\"") || !key.EndsWith("\"") || !value.StartsWith("\"") || !value.EndsWith("\""))
+                {
+                    return false;
+                }  
+            }
+
+            return true;
+        }
+
+        return true;
+   
+}
